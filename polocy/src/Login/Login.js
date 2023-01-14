20c1c7707212c78445ceb6a5e94d1737111e5353
sumit-krk <kushakk007@gmail.com>
2023-01-14 18:48:09 +0530

    Login component

---
+++ polocy/src/Login/Login.js
@@ -0,0 +1,15 @@
+const Login=()=>{
+    return <div>
+        <from>
+            <h1>Login</h1>
+            <div>
+                <input placeholder="User Name" style={{height:'20px',margin:'0px 5px'}} />
+                <input placeholder="Password" style={{height:'20px',margin:'0px 5px'}}/>
+            </div>
+            <div>
+                <button style={{padding:'5px',margin:'5px 5px'}}>Login</button>
+            </div>
+        </from>
+    </div>
+}
+export default Login;
