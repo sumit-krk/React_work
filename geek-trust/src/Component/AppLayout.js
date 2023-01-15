50dce7e0dd2c2427892865c377b478ffa39ac16f
sumit-krk <kushakk007@gmail.com>
2023-01-15 18:50:40 +0530

    All component under in app layout

---
+++ geek-trust/src/Component/AppLayout.js
@@ -0,0 +1,9 @@
+import Header from "./Header";
+import Search from "./SearchComponent/Search";
+const AppLayout=()=>{
+    return <>
+        <Header />
+        <Search />
+    </>
+}
+ export default AppLayout;
