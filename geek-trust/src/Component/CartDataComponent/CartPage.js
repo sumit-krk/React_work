ea7b6467478a6cf7cc332580fb75f3290a0a2a5e
sumit-krk <kushakk007@gmail.com>
2023-01-25 16:57:07 +0530

    cart data maped in cart component

--- geek-trust/src/Component/CartDataComponent/CartPage.js
+++ geek-trust/src/Component/CartDataComponent/CartPage.js
@@ -1,6 +1,45 @@
+import { useSelector } from "react-redux";
+
 const CartPage=()=>{
-    return <>
+    let data=useSelector((state)=> state.cartData);
+    console.log("cartData",data)
+    return (
+      <>
         <h1>Cart Data</h1>
-    </>
+        <div style={{}}>
+          {data.map((e) => {
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  borderBottom: "1px solid red",
+                  width: "400px",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                }}
+              >
+                <img height={150} width={150} src={e.imageURL} />
+                <div>
+                  <div>{e.price}</div>
+                  <div>{e.name}</div>
+                </div>
+                <div>
+                  <div>
+                    <button>+</button>
+                  </div>
+                  <div>1</div>
+                  <div>
+                    <button>-</button>
+                  </div>
+                </div>
+                <div>
+                  <button>Remove</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
 }
 export default CartPage;
--- geek-trust/src/Component/Header/Header.js
+++ geek-trust/src/Component/Header/Header.js
@@ -1,20 +1,25 @@
 import styles from './Header.module.css'
 import {useSelector} from 'react-redux'
+import { Link } from 'react-router-dom';
 const Header=()=>{
   let data = useSelector((state)=>state.cartData);
     return (
       <div className={styles.header}>
         <div>TeeRex Store</div>
-        <div>
-          <i
-            className="fa fa-shopping-cart"
-            aria-hidden="true"
-            style={{ fontSize: "35px", cursor: "pointer" }}
-          >
-            <span id="cart_item_count"></span>
-            <span style={{position:'absolute',top:'0px'}}>{data.length}</span>
-          </i>
-        </div>
+        <Link to="/CartPage">
+          <div>
+            <i
+              className="fa fa-shopping-cart"
+              aria-hidden="true"
+              style={{ fontSize: "35px", cursor: "pointer" }}
+            >
+              <span id="cart_item_count"></span>
+              <span style={{ position: "absolute", top: "0px" }}>
+                {data.length}
+              </span>
+            </i>
+          </div>
+        </Link>
       </div>
     );
 }
