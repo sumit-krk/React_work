5ca589378e363d3c93492a7ddcb38a23b4150e9e
sumit-krk <kushakk007@gmail.com>
2023-02-01 20:27:05 +0530

    all fileds rendered in ui

--- admin-dashboard/src/component/TableRowComponent.js
+++ admin-dashboard/src/component/TableRowComponent.js
@@ -7,6 +7,10 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
+import EditLocationAltIcon from '@mui/icons-material/EditLocationAlt';
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import IconButton from "@mui/material/IconButton";
 
 const head=["Name","Email","Role","Actions"]
 const TableRowComponent=({dashboardFilds})=>{
@@ -19,8 +23,8 @@ const TableRowComponent=({dashboardFilds})=>{
               <TableCell padding="checkbox">
                 <Checkbox
                   color="primary"
-                //   onClick={handleSelectAllClick}
-                //   checked={selectAll}
+                  //   onClick={handleSelectAllClick}
+                  //   checked={selectAll}
                 />
               </TableCell>
               {head.map((header) => (
@@ -30,6 +34,42 @@ const TableRowComponent=({dashboardFilds})=>{
               ))}
             </TableRow>
           </TableHead>
+          <TableBody>
+            {dashboardFilds.fild.map((user) => (
+              <TableRow
+                key={user.id}
+                // sx={{
+                //   ...(isSelected(user.name) && { backgroundColor: "#D3D3D3" }),
+                // }}
+                // onClick={(event) => handleClick(event, user.name)}
+              >
+                <TableCell padding="checkbox">
+                  <Checkbox
+                    color="primary"
+                    // checked={isSelected(user.name)}
+                    // onClick={(event) => handleClick(event, user.name)}
+                  />
+                </TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.role}</TableCell>
+                <TableCell>
+                  <IconButton
+                    color="primary"
+                    // onClick={() => handleEdit(user.id)}
+                  >
+                    <EditLocationAltIcon />
+                  </IconButton>
+                  <IconButton
+                    color="error"
+                    // onClick={() => handleDelete(user.id)}
+                  >
+                    <DeleteOutlineIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
         </Table>
       </TableContainer>
     );
