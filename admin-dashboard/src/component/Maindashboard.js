467b21d59fab9b566206384d5926e10f1939a89f
sumit-krk <kushakk007@gmail.com>
2023-02-01 21:49:54 +0530

    try to adding pagination

--- admin-dashboard/src/component/Maindashboard.js
+++ admin-dashboard/src/component/Maindashboard.js
@@ -3,12 +3,21 @@ import { useDispatch } from "react-redux";
 import { getAllfileds } from "../redux/action";
 import { useSelector } from 'react-redux';
 import TableRowComponent from './TableRowComponent';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 const Maindashboard=()=>{
     const dispatch=useDispatch();
     useEffect(()=>{dispatch(getAllfileds())},[])
     const { dashboardFilds }=useSelector((state)=>state);
-    return <div>
+    return (
+      <div>
         <TableRowComponent dashboardFilds={dashboardFilds} />
-    </div>
+        <div style={{display:'flex',justifyContent:'center'}}>
+          <Stack spacing={2}>
+            <Pagination count={10} showFirstButton showLastButton />
+          </Stack>
+        </div>
+      </div>
+    );
 }
 export default Maindashboard;
