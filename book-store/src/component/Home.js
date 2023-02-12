9b682fb07e945f7644d4d04aff37ad9fa8505d55
sumit-krk <kushakk007@gmail.com>
2023-02-12 20:32:26 +0530

    react slick working fine

---
+++ book-store/src/component/Home.js
@@ -0,0 +1,66 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import React, { Component } from "react";
+import Slider from "react-slick";
+import './Home.css';
+
+const SimpleSlider=()=> {
+
+    function SampleNextArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+          <div
+            className={className}
+            style={{ ...style, height:'30px', width:'30px', display: "flex", alignItems:'center', justifyContent:'center', background: "#d92e28" }}
+            onClick={onClick}
+          />
+        );
+      }
+      
+      function SamplePrevArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+          <div
+            className={className}
+            style={{ ...style, height:'30px', width:'30px', display: "flex", alignItems:'center', justifyContent:'center', background: "#d92e28" }}
+            onClick={onClick}
+          >
+             {/* <FontAwesomeIcon icon="fa-solid fa-chevron-right" /> */}
+          </div>
+        );
+      }
+    const settings = {
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: <SamplePrevArrow />,
+      nextArrow: <SampleNextArrow />,
+    };
+    return (
+      <>
+        <div> <img src="https://www.bookswagon.com/images/promotionimages/web/1_alltimefavourite.jpg?v=1.6" style={{width:'100%',height:'100px'}} /> </div>
+        <Slider {...settings}>
+          <div>
+            <img src='https://www.bookswagon.com/images/bannerimages/81_inr.jpg?v=1.4' style={{width:'100%',height:'500px'}} />
+          </div>
+          <div>
+            <img src='https://www.bookswagon.com/images/bannerimages/80_inr.jpg?v=1.4' style={{width:'100%',height:'500px'}} />
+          </div>
+          <div>
+            <img src='https://www.bookswagon.com/images/bannerimages/79_inr.jpg?v=1.4' style={{width:'100%',height:'500px'}} />
+          </div>
+          <div>
+            <img src='https://www.bookswagon.com/images/bannerimages/82_inr.jpg?v=1.4' style={{width:'100%',height:'500px'}} />
+          </div>
+          <div>
+            <img src='https://www.bookswagon.com/images/bannerimages/83_inr.jpg?v=1.4' style={{width:'100%',height:'500px'}} />
+          </div>
+          <div>
+            <img src='https://www.bookswagon.com/images/bannerimages/84_inr.jpg?v=1.4' style={{width:'100%',height:'500px'}} />
+          </div>
+        </Slider>
+      </>
+    );
+}
+export default SimpleSlider;
