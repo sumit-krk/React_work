691ed3b2fd81fd59fdb0d2fad1e6ec80a199ca9a
sumit-krk <kushakk007@gmail.com>
2023-02-13 23:37:53 +0530

    some styling in header

--- book-store/src/component/Header.js
+++ book-store/src/component/Header.js
@@ -1,15 +1,19 @@
 import {FaRegUserCircle} from 'react-icons/fa';
 import {RxDividerVertical} from 'react-icons/rx';
 import {AiOutlineHeart} from 'react-icons/ai'
+import { FaSearch } from "react-icons/fa";
 import './Header.css';
 const Header=()=>{
     return (
         <div className="header">
           <img src="https://d2g9wbak88g7ch.cloudfront.net/staticimages/logo-new.png" style={{padding:'0px 0px 0px 40px'}} />
-          <input placeholder="Search by Title, Author, Publisher or ISBN" style={{width:'400px',height:'30px',border:'1px solid red'}}/>
+          <div style={{display:'flex'}}>
+            <input placeholder="Search by Title, Author, Publisher or ISBN" style={{width:'400px',height:'30px',border:'1px solid red'}}/>
+            <FaSearch style={{backgroundColor:'red',color:'white',padding:'9px',display:'flex',cursor:'pointer'}} />
+          </div>
           <div style={{display:'flex',alignItems:'center',padding:'0px 60px 0px 0px'}}>
             <FaRegUserCircle style={{color:'red',display:'flex',fontSize:'30px'}} />
-            <p1 style={{padding:'0px 5px 0px 0px'}}>My Account</p1>
+            <p1 style={{padding:'0px 5px 0px 0px'}}> My Account</p1>
             <RxDividerVertical style={{color:'red',fontSize:'30px'}} />  
             <AiOutlineHeart style={{color:'red',fontSize:'30px'}} />      
           </div>
