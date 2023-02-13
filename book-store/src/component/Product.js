c482b7c0b74e8326154d958e5211d16105b87eb3
sumit-krk <kushakk007@gmail.com>
2023-02-13 23:36:52 +0530

    added filter by price

--- book-store/src/component/Product.js
+++ book-store/src/component/Product.js
@@ -3,6 +3,8 @@ import { gatAllBookData } from "../Redux/action";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { BiRupee } from "react-icons/bi";
+import { AiFillStar } from "react-icons/ai";
+import Header from "./Header";
 const Product=()=>{
     const dispatch=useDispatch();
     const {currentBookData} = useSelector((state)=> state.BookData);
@@ -10,11 +12,37 @@ const Product=()=>{
     useEffect(()=>{
         dispatch(gatAllBookData())
     },[])
+
+    const SelectFilter=(e)=>{
+        const {checked,value}=e.target;
+            //  if(checked){
+            //   setSelectedCategory([...selectedCategory,value]);
+              
+            //  }
+            //  else{
+            //     setSelectedCategory([...selectedCategory?.filter((e)=>e!=value)])
+            //  }
+    }
+
     return (
       <>
+        <Header />
         <div style={{display:'flex'}}>
           <div style={{borderRight:'1px solid black',width:'15%'}}>
-            <h1>Filters</h1>
+            <h2 style={{display:'flex',justifyContent:'center',borderBottom:'1px solid black'}}>Filters</h2>
+            {/* price filter box */}
+            <div className="checkBoxDiv">
+                <div>Price</div>
+                <div style={{display:'flex',alignItems:'center'}}>
+                    <input type="checkbox" name="" onChange={SelectFilter} value={'250'} /><label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><BiRupee /></span>100 - 300</label>
+                </div>
+                <div style={{display:'flex',alignItems:'center'}}>
+                <input type="checkbox" name="" onChange={SelectFilter} value={"251"} /> <label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><BiRupee /></span>301 - 600</label>
+                </div>
+                <div style={{display:'flex',alignItems:'center'}}>
+                <input type="checkbox" name="" onChange={SelectFilter} value={'450'} /> <label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><BiRupee /></span>601 - 1000</label>
+                </div>
+            </div>
           </div>
           <div style={{ display: "flex", flexWrap: "wrap",width:'85%',justifyContent:'space-around' }}>
             {currentBookData.map((e) => {
@@ -32,7 +60,12 @@ const Product=()=>{
                     style={{ width: "100%", height: "250px" }}
                   />
                   <div>{e.title.split(" ").slice(0,4).join(" ")}</div>
-                  <div style={{display:'flex',alignItems:'center',border:'1px solid green'}}><span style={{display:'flex'}}><BiRupee /></span><span>{e.price}</span></div>
+                  <div style={{display:'flex',alignItems:'center',justifyContent:'space-around',border:'1px solid green'}}>
+                    <div style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><BiRupee /></span><span>{String(e.price).split(".")}</span></div>
+                    <div style={{display:'flex',alignItems:'center',backgroundColor:'green'}}>
+                        <span>{e.rating}</span><span style={{display:'flex',color:'white'}}><AiFillStar /></span>
+                    </div>
+                  </div>
                 </div>
               );
             })}
