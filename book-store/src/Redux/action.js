1f198377f5a6a6b8ebbc88afc4406efac60b1a09
sumit-krk <kushakk007@gmail.com>
2023-02-16 03:03:25 +0530

    action file

--- book-store/src/Redux/action.js
+++ book-store/src/Redux/action.js
@@ -1,7 +1,44 @@
-import {GET_BOOK_DATA} from './constant'
+import {GET_BOOK_DATA, ADD_TO_CART, REMOVE_FROM_CART, INCREASE_QUENTITY, DECRISE_QUENTITY, FILTER_PRODUCT} from './constant'
 
 export const gatAllBookData=()=>{
     return {
         type:GET_BOOK_DATA,
+    }
+}
+
+export const addToCart=(data)=>{
+    console.log(data)
+    return {
+        type:ADD_TO_CART,
+        data
+    }
+}
+
+export const removeFromCart=(data)=>{
+    console.log(data)
+    return{
+        type:REMOVE_FROM_CART,
+        data,
+    }
+}
+
+export const increaseQuantity=(data)=>{
+    return{
+        type:INCREASE_QUENTITY,
+        data
+    }
+}
+
+export const decriseQuantity=(data)=>{
+    return{
+        type:DECRISE_QUENTITY,
+        data
+    }
+}
+
+export const filterProduct=(data)=>{
+    return{
+        type:FILTER_PRODUCT,
+        data
     }
 }
