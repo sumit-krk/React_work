86ff807fcfe14276a8fc7d4fe1a18858fdffa5fc
sumit-krk <kushakk007@gmail.com>
2023-02-16 03:00:53 +0530

    filter part done

--- book-store/src/component/Filter.js
+++ book-store/src/component/Filter.js
@@ -2,17 +2,28 @@ import { BiRupee } from "react-icons/bi";
 import { BsPaperclip } from "react-icons/bs";
 import { RiHardDrive2Fill } from "react-icons/ri";
 import { AiFillStar } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { filterProduct } from "../Redux/action";
+import { useDispatch } from "react-redux";
 const Filter=()=>{
+    // console.log("bing",bringDataFromChild)
+    const dispatch=useDispatch();
+    const [selectedCategory,setSelectedCategory]=useState([]);
     const SelectFilter=(e)=>{
         const {checked,value}=e.target;
-            //  if(checked){
-            //   setSelectedCategory([...selectedCategory,value]);
+             if(checked){
+              setSelectedCategory([...selectedCategory,value]);
               
-            //  }
-            //  else{
-            //     setSelectedCategory([...selectedCategory?.filter((e)=>e!=value)])
-            //  }
+             }
+             else{
+                setSelectedCategory([...selectedCategory?.filter((e)=>e!=value)])
+             }  
     }
+
+    useEffect(()=>{
+        dispatch(filterProduct(selectedCategory))
+     },[selectedCategory])
+    console.log("print",selectedCategory);
     return (
         <div style={{borderRight:'1px solid black',width:'15%'}}>
             <h2 style={{display:'flex',justifyContent:'center',borderBottom:'1px solid black'}}>Filters</h2>
@@ -22,13 +33,13 @@ const Filter=()=>{
             <div className="checkBoxDiv" style={{marginLeft:'20px'}}>
                 <h4>Price</h4>
                 <div style={{display:'flex',alignItems:'center'}}>
-                    <input type="checkbox" name="" onChange={SelectFilter} value={'250'} /><label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><BiRupee /></span>100 - 300</label>
+                    <input type="checkbox" name="" onChange={SelectFilter} value={'300'} /><label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><BiRupee /></span>100 - 300</label>
                 </div>
                 <div style={{display:'flex',alignItems:'center'}}>
-                <input type="checkbox" name="" onChange={SelectFilter} value={"251"} /> <label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><BiRupee /></span>301 - 600</label>
+                <input type="checkbox" name="" onChange={SelectFilter} value={"600"} /> <label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><BiRupee /></span>301 - 600</label>
                 </div>
                 <div style={{display:'flex',alignItems:'center'}}>
-                <input type="checkbox" name="" onChange={SelectFilter} value={'450'} /> <label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><BiRupee /></span>601 - 1000</label>
+                <input type="checkbox" name="" onChange={SelectFilter} value={'1000'} /> <label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><BiRupee /></span>601 - 1000</label>
                 </div>
             </div>
 
@@ -37,10 +48,10 @@ const Filter=()=>{
             <div className="checkBoxDiv" style={{marginLeft:'20px'}}>
                 <h4>Format</h4>
                 <div style={{display:'flex',alignItems:'center'}}>
-                    <input type="checkbox" name="" onChange={SelectFilter} value={'250'} /><label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><RiHardDrive2Fill /></span>Hardcover</label>
+                    <input type="checkbox" name="" onChange={SelectFilter} value={'Hardcover'} /><label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><RiHardDrive2Fill /></span>Hardcover</label>
                 </div>
                 <div style={{display:'flex',alignItems:'center'}}>
-                <input type="checkbox" name="" onChange={SelectFilter} value={"251"} /> <label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><BsPaperclip /></span>Paperback</label>
+                <input type="checkbox" name="" onChange={SelectFilter} value={"Paperback"} /> <label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><BsPaperclip /></span>Paperback</label>
                 </div>
             </div>
 
@@ -49,10 +60,10 @@ const Filter=()=>{
             <div className="checkBoxDiv" style={{marginLeft:'20px'}}>
                 <h4>Rating</h4>
                 <div style={{display:'flex',alignItems:'center'}}>
-                    <input type="checkbox" name="" onChange={SelectFilter} value={'250'} /><label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><AiFillStar /></span>3 & above</label>
+                    <input type="checkbox" name="" onChange={SelectFilter} value={'3'} /><label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><AiFillStar /></span>3 & above</label>
                 </div>
                 <div style={{display:'flex',alignItems:'center'}}>
-                <input type="checkbox" name="" onChange={SelectFilter} value={"251"} /> <label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><AiFillStar /></span>4 & above</label>
+                <input type="checkbox" name="" onChange={SelectFilter} value={"4"} /> <label htmlFor="" style={{display:'flex',alignItems:'center'}}><span style={{display:'flex'}}><AiFillStar /></span>4 & above</label>
                 </div>
             </div>
           </div>
