768ef7a48fca764ce4af7bb90e5796b5c8d20e33
sumit-krk <kushakk007@gmail.com>
2023-02-16 21:22:40 +0530

    combined search and product listing

---
+++ geek-trust/src/Component/ProductList&CartComponent/ProductSearch.js
@@ -0,0 +1,11 @@
+import ProductListing from "../ProductListingComponent/ProductListing";
+import Search from "../SearchComponent/Search";
+const ProductSearch=()=>{
+    return (
+        <>
+            <Search />
+            <ProductListing />
+        </>
+    )
+}
+export default ProductSearch;
