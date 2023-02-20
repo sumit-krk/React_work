c6643b304a6eb0f2941043b8335872cee06c498e
sumit-krk <kushakk007@gmail.com>
2023-02-20 21:43:33 +0530

    explore componentDidmout and componentUpdate phase

--- get-classy/src/component/Child.js
+++ get-classy/src/component/Child.js
@@ -1,14 +1,28 @@
 import React from "react";
 class Child extends React.Component {
     constructor(props){
+        console.log("whenever component will run I will run first (constructor function)")
         super(props)
-            this.state={    /* created state */
+            this.state={    /* created state in class component */
                 count:this.props.num, /* created count variable in state */
                 string:'', /* created string variable in state */
                 mul:1,
             }
     }
+
+    componentDidMount() {
+        console.log("After constructor function and render method, component did mount will be call");
+        /* best place to call api here */
+        /* it will be run only one time when ever component will be render first time (commit phase)*/
+    }
+
+    componentDidUpdate(){
+        console.log("updating phase");
+        /*it will not run first time when component will be render, but it will run when ever state gets update in our component during the commit phase*/
+    }
+
     render(){
+        console.log("after constructor function render method will be run")
         return <>
             <div style={{border:'1px solid red'}}>
                 <button onClick={()=>{this.setState({
