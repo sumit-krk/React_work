3e72137f208a583ffc7f4a8cf7d26e0882897441
sumit-krk <kushakk007@gmail.com>
2023-02-20 20:24:59 +0530

    parent component by calss component

---
+++ get-classy/src/component/Parent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Child from "./Child";
+class Parent extends React.Component {
+    render(){
+        return <>
+            <h1>Counter</h1>
+            <Child />
+        </>
+    }
+}
+export default Parent;
