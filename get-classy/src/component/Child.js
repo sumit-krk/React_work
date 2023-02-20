74c7502afe33829cb1db4cc756de907fa1c343e9
sumit-krk <kushakk007@gmail.com>
2023-02-20 20:50:16 +0530

    created string variable in state

--- get-classy/src/component/Child.js
+++ get-classy/src/component/Child.js
@@ -2,17 +2,22 @@ import React from "react";
 class Child extends React.Component {
     constructor(props){
         super(props)
-            this.state={
-                count:this.props.num
+            this.state={    /* created state */
+                count:this.props.num, /* created count variable in state */
+                string:'' /* created string variable in state */
             }
     }
     render(){
         return <>
-            <div>
+            <div style={{border:'1px solid red'}}>
                 <button>+</button>
                 {/* <div>{this.props.num}</div> excess props directly */}
                 <div>{this.state.count}</div>   {/*firstly props store in store in state then excess here*/}
                 <button>-</button>
+            </div>
+            <div style={{border:'1px solid green'}}>
+                <input value={this.state.string} />
+                <div>{this.state.string}</div>
             </div>
         </>
     }
